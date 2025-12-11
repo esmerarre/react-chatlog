257a60a1675b68da23a46d83f17082d5807541cc
Esmeralda Arreguin-Martinez <esmerarre@gmail.com>
2025-12-10 19:23:32 -0800

    wave 1-one chat added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry';
+// import ChatData from './data/messages.json';
 
 const App = () => {
+  const chatData = {
+    'id': 1,
+    'sender': 'Vladimir',
+    'body': 'why are you arguing with me',
+    'timeStamp': '2018-05-29T22:49:06+00:00',
+    'liked': false
+  };
+
   return (
     <div id="App">
       <header>
@@ -9,6 +19,7 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry sender={chatData.sender} body={chatData.body} timeStamp={chatData.timeStamp}></ChatEntry>
       </main>
     </div>
   );
--- src/components/ChatEntry.jsx
+++ src/components/ChatEntry.jsx
@@ -1,21 +1,36 @@
 import './ChatEntry.css';
+import TimeStamp from './TimeStamp';
+// import PropTypes from 'prop-types';
+/* we need name of sender to be displayed above bubble
+text of the message to be placed inside bubble
+need timestamp of when the text was sent, slightly below body of message and still within the bubble 
+the like button is included here
+data needed:
+    "sender":"Estragon",
+    "body":"Because you are wrong.",
+    "timeStamp":"2018-05-29T22:49:33+00:00",
+    "liked": false
 
-const ChatEntry = () => {
+*/
+
+const ChatEntry = (props) => {
   return (
     // Replace the outer tag name with a semantic element that fits our use case
-    <replace-with-relevant-semantic-element className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+    <div className="chat-entry local">
+      <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>{props.body}</p>
+        <p className="entry-time"><TimeStamp time={props.timeStamp} /></p>
         <button className="like">🤍</button>
       </section>
-    </replace-with-relevant-semantic-element>
+    </div>
   );
 };
 
-ChatEntry.propTypes = {
-  // Fill with correct proptypes
-};
+// ChatEntry.propTypes = {
+//   sender: PropTypes.string.isRequired,
+//   body: PropTypes.string.isRequired,
+//   timeStamp: PropTypes.string.isRequired
+// };
 
 export default ChatEntry;
